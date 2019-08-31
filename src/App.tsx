39781293a6b4c81637eeb89ccd0be837a4e5e178
wwSchrader <wwschrader@gmail.com>
2019-08-30 21:45:58 -0700

    Add projects component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import SplashSection from './SplashSection';
 import SkillsComponent from './SkillsComponent';
+import ProjectsComponent from './ProjectsComponent';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <SplashSection />
       <SkillsComponent />
+      <ProjectsComponent />
     </div>
   );
 }
---
+++ src/ProjectsComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectsComponent: React.FC = () => {
+  return (
+    <div className='ProjectsComponent'>
+      <h3>Projects Component</h3>
+    </div>
+  );
+}
+
+export default ProjectsComponent;
