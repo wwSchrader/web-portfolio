5d638fb89878dbb83dbfb7b6eb6da3c2e5b977a0
wwSchrader <wwschrader@gmail.com>
2019-08-31 00:15:42 -0700

    Add SplashSection content

--- src/SplashSection.tsx
+++ src/SplashSection.tsx
@@ -9,7 +9,11 @@ const  SplashSection: React.FC = () => {
   console.log(ParticlesParam);
   return (
     <div className='SplashSection'>
-      <ReactParticles className='ReactParticles' width='100%' height='100%' params={particlesParam}/>
+      <ReactParticles className='ReactParticles' width='100%' height='100%' params={particlesParam} />
+      <div className='SplashSection-content'>
+        <h2>Howdy! I'm Warren Schrader.</h2>
+        <h3>I'm a full-stack web developer.</h3>
+      </div>
     </div>
   );
 }
