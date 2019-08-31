14573c63b04b540fcdb773c94b86e0a1353805ca
wwSchrader <wwschrader@gmail.com>
2019-08-30 21:37:17 -0700

    Add a splash section component

---
+++ src/SplashSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const  SplashSection: React.FC = () => {
+  return (
+    <div className='SplashSection'>
+      <h3>Splash Section</h3>
+    </div>
+  );
+}
+
+export default SplashSection;
