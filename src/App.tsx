ca7e4dd48bd0501f98eb1e8d7da2a04eb632d4a7
wwSchrader <wwschrader@gmail.com>
2019-08-30 21:42:38 -0700

    Add Skills Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import SplashSection from './SplashSection';
+import SkillsComponent from './SkillsComponent';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <SplashSection />
+      <SkillsComponent />
     </div>
   );
 }
---
+++ src/SkillsComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SkillsComponent: React.FC = () => {
+  return (
+    <div className='SkillsComponent'>
+      <h3>Skills Component</h3>
+    </div>
+  );
+}
+
+export default SkillsComponent;
