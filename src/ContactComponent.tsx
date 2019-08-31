d96430e976624a6e4ff66a25474a0707b4766a59
wwSchrader <wwschrader@gmail.com>
2019-08-30 22:07:42 -0700

    Add Contact Component

---
+++ src/ContactComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactComponent: React.FC = () => {
+  return (
+    <div className='ContactComponent'>
+      <h3>Contact Component</h3>
+    </div>
+  );
+}
+
+export default ContactComponent;
