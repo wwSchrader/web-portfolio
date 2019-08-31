3ab3a24a804c49d13736edb71e2b4dc22910a9b3
wwSchrader <wwschrader@gmail.com>
2019-08-30 23:30:31 -0700

    Add react particles js to splash section

--- src/SplashSection.tsx
+++ src/SplashSection.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import ReactParticles from 'react-particles-js';
+import ParticlesParam from './ParticlesParam.json';
+import './SplashSection.css';
+
+const particlesParam = ParticlesParam as any;
 
 const  SplashSection: React.FC = () => {
+  console.log(ParticlesParam);
   return (
     <div className='SplashSection'>
-      <h3>Splash Section</h3>
+      <ReactParticles className='ReactParticles' width='100%' height='100%' params={particlesParam}/>
     </div>
   );
 }
