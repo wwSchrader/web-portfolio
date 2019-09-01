4ca7a9ac07e5cbffd39477339901695e1c63c81e
wwSchrader <wwschrader@gmail.com>
2019-09-01 16:01:40 -0700

    Add a text loop in spash section

--- src/SplashSection.tsx
+++ src/SplashSection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactParticles from 'react-particles-js';
 import ParticlesParam from './ParticlesParam.json';
 import './SplashSection.css';
+import TextLoop from 'react-text-loop';
 
 const particlesParam = ParticlesParam as any;
 
@@ -12,7 +13,15 @@ const  SplashSection: React.FC = () => {
       <ReactParticles className='ReactParticles' width='100%' height='100%' params={particlesParam} />
       <div className='SplashSection-content'>
         <h2>Howdy! I'm Warren Schrader.</h2>
-        <h3>I'm a full-stack web developer.</h3>
+        <h3>
+          I'm a <span>
+            <TextLoop>
+              <span>front-end</span>
+              <span>back-end</span>
+              <span>full-stack</span>
+            </TextLoop> 
+          </span> web developer.
+        </h3>
       </div>
     </div>
   );
