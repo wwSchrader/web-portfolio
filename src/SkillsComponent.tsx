387313b4ba71593b165e37569a21bf17613d1a50
wwSchrader <wwschrader@gmail.com>
2019-09-01 20:33:10 -0700

    Add logos to the Skills Component

--- src/SkillsComponent.tsx
+++ src/SkillsComponent.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import './SkillsComponent.css';
 
 const SkillsComponent: React.FC = () => {
   return (
     <div className='SkillsComponent'>
       <h3>Skills Component</h3>
+      <div>
+        <img alt='' src='./images/HTML5_logo_and_wordmark.svg' />
+        <img alt='' src='./images/CSS3_logo_and_wordmark.svg' />
+        <img alt='' src='./images/js.png' />
+        <img alt='' src='./images/express-facebook-share.png' />
+        <img alt='' src='./images/iconfinder_react.js_logo_1174949.svg'/>
+        <img alt='' src='./images/nodejs-new-pantone-black.png' />
+      </div>
+      <div>
+        <img alt='' src='./images/mongodb-logo-rgb-j6w271g1xn.jpg' />
+        <img alt='' src='./images/Boostrap_logo.svg' />
+        <img alt='' src='./images/mongoose-logo.png' />
+        <img alt='' src='./images/socket-io.svg' />
+      </div>
+      <div>
+        <img alt='' src='./images/jest-logo.svg' />
+        <img alt='' src='./images/mocha-logo.svg' />
+        <img alt='' src='./images/chai-logo.png' />
+        <img alt='' src='./images/passport-logo.png' />
+        <img alt='' src='./images/Java_logo.svg' />
+        <img alt='' src='./images/sqlite-logo.svg' />
+      </div>
+      <div>
+        <img alt='' src='./images/Redis_Logo.svg' />
+        <img alt='' src='./images/firebase-logo.svg' />
+        <img alt='' src='./images/heroku-logo.svg' />
+        <img alt='' src='./images/github-logo.png' />
+        <img alt='' src='./images/Git-logo.svg' />
+        <img alt='' src='./images/redux-logo.png' />
+        <img alt='' src='./images/angular.svg' />
+      </div>
     </div>
   );
 }
