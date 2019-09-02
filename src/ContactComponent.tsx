ce6d6bf651c97c016cc79277b22198bd471dfc6c
wwSchrader <wwschrader@gmail.com>
2019-09-01 23:21:05 -0700

    Add contact form to Contact Component

--- src/ContactComponent.tsx
+++ src/ContactComponent.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './ContactComponent.css';
 
 const ContactComponent: React.FC = () => {
   return (
     <div className='ContactComponent'>
-      <h3>Contact Component</h3>
+      <h3>Contact Me</h3>
+      <form className='contact-form' action="https://formspree.io/email@domain.tld" method="POST">
+        Name:
+        <input type='text' name='name' required/>
+        Email:
+        <input type='email' required/>
+        Subject:
+        <input type='text' name='subject' required/>
+        Message:
+        <textarea name='message' rows={5} required/>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
