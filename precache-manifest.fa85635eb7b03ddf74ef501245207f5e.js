b2a92547f9ab1753b1bad93caecd41246de6abab
wwSchrader <wwschrader@gmail.com>
2019-09-01 23:56:28 -0700

    Updates

---
+++ precache-manifest.fa85635eb7b03ddf74ef501245207f5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d872a0365c7dbd99d42706be519884",
+    "url": "/web-portfolio/index.html"
+  },
+  {
+    "revision": "dc32f7867e7ab5d2defc",
+    "url": "/web-portfolio/static/css/main.5a2b4dc6.chunk.css"
+  },
+  {
+    "revision": "daca54b465c2a04e6150",
+    "url": "/web-portfolio/static/js/2.3a02e2c8.chunk.js"
+  },
+  {
+    "revision": "dc32f7867e7ab5d2defc",
+    "url": "/web-portfolio/static/js/main.c9d5bcec.chunk.js"
+  },
+  {
+    "revision": "3c9aa42e652b6d0cd1b7",
+    "url": "/web-portfolio/static/js/runtime~main.ed58379f.js"
+  }
+]);
