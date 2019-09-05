3b890018561da81d83a4b61ef97245a28191b655
wwSchrader <wwschrader@gmail.com>
2019-09-04 22:47:02 -0700

    Add categories in the skills component and formated categories

--- src/SkillsComponent.tsx
+++ src/SkillsComponent.tsx
@@ -5,36 +5,53 @@ const SkillsComponent: React.FC = () => {
   return (
     <div className='SkillsComponent'>
       <h3>Skills Component</h3>
-      <div>
-        <img alt='' src='./images/HTML5_logo_and_wordmark.svg' />
-        <img alt='' src='./images/CSS3_logo_and_wordmark.svg' />
-        <img alt='' src='./images/js.png' />
-        <img alt='' src='./images/express-facebook-share.png' />
-        <img alt='' src='./images/iconfinder_react.js_logo_1174949.svg'/>
-        <img alt='' src='./images/nodejs-new-pantone-black.png' />
+      <div className='skills-programlang'>
+        <h2 className='skills-programlang-header'>Languages</h2>
+        <div>
+          <img alt='' src='./images/HTML5_logo_and_wordmark.svg' />
+          <img alt='' src='./images/CSS3_logo_and_wordmark.svg' />
+          <img alt='' src='./images/js.png' />
+          <img alt='' src='./images/Java_logo.svg' />
+        </div>
       </div>
       <div>
-        <img alt='' src='./images/mongodb-logo-rgb-j6w271g1xn.jpg' />
-        <img alt='' src='./images/Boostrap_logo.svg' />
-        <img alt='' src='./images/mongoose-logo.png' />
-        <img alt='' src='./images/socket-io.svg' />
+        <div>
+          <img alt='' src='./images/nodejs-new-pantone-black.png' />
+          <img alt='' src='./images/express-facebook-share.png' />
+          <img alt='' src='./images/iconfinder_react.js_logo_1174949.svg'/>
+          <img alt='' src='./images/angular.svg' />
+        </div>
+        <h2>Frameworks</h2>
       </div>
       <div>
+        <h2>Databases</h2>
+        <div>
+          <img alt='' src='./images/mongodb-logo-rgb-j6w271g1xn.jpg' />
+          <img alt='' src='./images/Redis_Logo.svg' />
+          <img alt='' src='./images/sqlite-logo.svg' />
+          <img alt='' src='./images/firebase-logo.svg' />
+        </div>
+      </div>
+      <div>
+        <div>
         <img alt='' src='./images/jest-logo.svg' />
-        <img alt='' src='./images/mocha-logo.svg' />
-        <img alt='' src='./images/chai-logo.png' />
-        <img alt='' src='./images/passport-logo.png' />
-        <img alt='' src='./images/Java_logo.svg' />
-        <img alt='' src='./images/sqlite-logo.svg' />
+          <img alt='' src='./images/mocha-logo.svg' />
+          <img alt='' src='./images/chai-logo.png' />
+        </div>
+        <h2>Test Libraries</h2>
       </div>
       <div>
-        <img alt='' src='./images/Redis_Logo.svg' />
-        <img alt='' src='./images/firebase-logo.svg' />
-        <img alt='' src='./images/heroku-logo.svg' />
-        <img alt='' src='./images/github-logo.png' />
-        <img alt='' src='./images/Git-logo.svg' />
-        <img alt='' src='./images/redux-logo.png' />
-        <img alt='' src='./images/angular.svg' />
+        <h2>Other</h2>
+        <div>
+          <img alt='' src='./images/heroku-logo.svg' />
+          <img alt='' src='./images/github-logo.png' />
+          <img alt='' src='./images/Git-logo.svg' />
+          <img alt='' src='./images/redux-logo.png' />
+          <img alt='' src='./images/Boostrap_logo.svg' />
+          <img alt='' src='./images/mongoose-logo.png' />
+          <img alt='' src='./images/socket-io.svg' />
+          <img alt='' src='./images/passport-logo.png' />
+        </div>
       </div>
     </div>
   );
