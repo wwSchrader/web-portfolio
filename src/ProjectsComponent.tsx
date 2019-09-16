86de09056d139ab6ca530ce702372347869795f7
wwSchrader <wwschrader@gmail.com>
2019-09-15 23:32:27 -0700

    Add images to the Projects component

--- src/ProjectsComponent.tsx
+++ src/ProjectsComponent.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './ProjectsComponent.css';
 
 const ProjectsComponent: React.FC = () => {
   return (
     <div className='ProjectsComponent'>
       <h3>Projects Component</h3>
+      <img className={'project-img'} src={'./images/beer-buds.png'} />
+      <img className={'project-img'} src={'./images/book-brain.png'} />
+      <img className={'project-img'} src={'./images/game-of-life.png'} />
+      <img className={'project-img'} src={'./images/howling.png'} />
+      <img className={'project-img'} src={'./images/react-roguelike.png'} />
+      <img className={'project-img'} src={'./images/voting-app.png'} />
     </div>
   );
 }
