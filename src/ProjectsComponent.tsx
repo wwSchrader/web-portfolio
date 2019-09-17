2b0bcc8fcbfdee6a16f87dc7c345c8cfd594a729
wwSchrader <wwschrader@gmail.com>
2019-09-16 23:40:15 -0700

    Update project component to list project names and buttons on each image

--- src/ProjectsComponent.tsx
+++ src/ProjectsComponent.tsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import './ProjectsComponent.css';
 
+const projects = [
+  {
+    name: 'Beer Buds',
+    url: './images/beer-buds.png',
+  },
+  {
+    name: 'Book Brain',
+    url: './images/book-brain.png',
+  },
+  {
+    name: 'Game of Life',
+    url: './images/game-of-life.png',
+  },
+  {
+    name: 'Howling',
+    url: './images/howling.png',
+  },
+  {
+    name: 'React Rougelike',
+    url: './images/react-roguelike.png',
+  },
+  {
+    name: 'Voting App',
+    url: './images/voting-app.png',
+  },
+];
+
 const ProjectsComponent: React.FC = () => {
   return (
-    <div className='ProjectsComponent'>
+    <div>
       <h3>Projects Component</h3>
-      <img className={'project-img'} src={'./images/beer-buds.png'} />
-      <img className={'project-img'} src={'./images/book-brain.png'} />
-      <img className={'project-img'} src={'./images/game-of-life.png'} />
-      <img className={'project-img'} src={'./images/howling.png'} />
-      <img className={'project-img'} src={'./images/react-roguelike.png'} />
-      <img className={'project-img'} src={'./images/voting-app.png'} />
+      <div className='projects-container'>
+        {projects.map((project) => (
+          <div className={'single-project-container'}>
+            <div className='project-image' style={{backgroundImage: 'url(' + project.url + ')'}}></div>
+            <h2 className={'project-header'}>{project.name}</h2>
+            <button className='project-button'>Click Here</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
